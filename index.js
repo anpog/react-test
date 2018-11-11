65b76dbba34b4f39086961efff4640e9c02671e7
anpog <42399314+anpog@users.noreply.github.com>
2018-11-11 13:07:59 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Anahit Poghosyan</h1>
+    <p>Product Designer</p>
+    <p>
+      Welcome to my portfolio. Look around, see if you're interested in any
+      particular project and drop me a line or two if you'd like to chat.
+    </p>
+    <Link to="/work/">My Work</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ work.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>My Work</h1>
+    <p>All my current projects on this page</p>
+    <Link to="/">Back</Link>
+  </div>
+)
+
+export default SecondPage
